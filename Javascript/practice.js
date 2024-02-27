e48ff0d9aa2faa7ee2a4a080bab3a7d568a9a329
HARSH MISHRA <harshmishr540@gmail.com>
2024-02-27 10:17:19 +0530

    Else if

---
+++ Javascript/practice.js
@@ -0,0 +1,20 @@
+let color="red";
+if(color === "red"){
+    console.log("Stop!");
+}
+if(color === "yellow"){
+    console.log("Start");
+}
+ if(color === "green"){
+    console.log("Go");
+} 
+let marks="80"
+if(marks >= "80"){
+    console.log("A");
+}
+else if(marks <=80 ){
+    console.log("B");
+}
+else if(marks <33){
+    console.log("f");
+}
