cdbc1f0273fdbbe651c14e3c0c7f78e01aeb6444
Harshmishra1909 <harshmishr540@gmail.com>
2024-02-27 11:36:13 +0530

    practice js

---
+++ Javascript/practice1.js
@@ -0,0 +1,16 @@
+let size="S";
+if(size === "xl" ){
+    console.log("price is 250");
+}
+ else if(size === "L"){
+    console.log("price is 200");
+}
+else if(size === "M"){
+    console.log("price is 100");
+}
+else if(size === "S"){
+    console.log("price is 50");
+}
+else{
+    console.log("Give your correct size");
+}
---
+++ Javascript/practice2.js
@@ -0,0 +1,10 @@
+let marks=32;
+if(marks >=33 ){
+    console.log("Pass");
+}
+if(marks >=80){
+    console.log("A")
+}
+else{
+    console.log("Better luck next time")
+}
